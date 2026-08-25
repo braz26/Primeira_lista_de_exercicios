d1b236ca6845252f0f89aacd91553f856dd3e30e
João Victor da Rocha Braz <jvb26@MacBook-Air-de-Joao.local>
2026-08-25 20:33:32 -0300

    exercicio1

---
+++ exercicio1.c
@@ -0,0 +1,13 @@
+#include <stdio.h> //importacao da biblioteca
+ 
+int main() {
+
+    int numero1= 10;
+    int numero2= 15;
+    int numero3= 20;
+    int numero4= 25;
+    int soma= numero1 + numero2 + numero3 + numero4;
+
+    printf("O valor da soma é: %d\n", soma);
+    return 0;
+}
