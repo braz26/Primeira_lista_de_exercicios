d23320f44a2fd7dba5fd12cc67526de8c6e85082
João Victor da Rocha Braz <jvb26@MacBook-Air-de-Joao.local>
2026-08-29 10:11:14 -0300

    exercicio14

--- exercicio14.c
+++ exercicio14.c
@@ -1,6 +1,26 @@
 #include <stdio.h> //importacao da biblioteca
-#include <math.h> // importacao da biblioteca para funcoes matematicas
 
-int main() {
-    int ;
-}
+    int calcular_idade(int ano_atual, int ano_nascimento) {
+        return ano_atual - ano_nascimento;
+    }
+    int calcular_idade_em_2050 (int ano_nascimento) {
+        return 2050 - ano_nascimento;
+    }
+    int main() {
+        int ano_atual, ano_nascimento;
+
+        printf("Digite o ano atual: ");
+        scanf("%d", & ano_atual);
+    
+        printf("Digite o ano de nascimento: ");
+        scanf("%d", & ano_nascimento);
+
+        if (ano_atual< ano_nascimento) {
+            printf("Erro: O ano de nascimento nao pode ser maior que o ano atual.\n");
+            return 1;
+        }
+            printf("a)Sua idade atual e: %d\n", calcular_idade(ano_atual, ano_nascimento));
+            printf("b) Sua idade em 2050 sera: %d\n", calcular_idade_em_2050(ano_nascimento));
+            return 0;
+        }
+    
